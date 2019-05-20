2e3646230462fc5ec9e5592d4cf7a440805317a2
Anrover <krosh347@gmail.com>
2019-05-20 22:53:18 +0500

    change demonize

--- 5/inputer.cpp
+++ 5/inputer.cpp
@@ -44,56 +44,50 @@ void skeleton_daemon() {
     umask(0);
     chdir("/");
 
-    //перенаправляем весь вывод в /dev/null
-    int devNull = open("/dev/null", O_WRONLY);
-    if(devNull == -1){
-        syslog(LOG_ERR, "Error in open('/dev/null',0)");
+    if (int fdnull = open("/dev/null", O_RDWR)) {   
+        dup2 (fdnull, STDIN_FILENO);
+        dup2 (fdnull, STDOUT_FILENO);
+        dup2 (fdnull, STDERR_FILENO);
+        close(fdnull);
+    }
+    else {
+        syslog(LOG_ERR, "Failed to open /dev/null");
         exit(EXIT_FAILURE);
     }
-    if(dup2(devNull, STDOUT_FILENO) == -1) {
-        syslog(LOG_ERR, "Error in dup2(devNull, STDOUT_FILENO)");
-        exit(EXIT_FAILURE);
-    }
-
-	// Пришлось перенаправлять вывод, потому как если его просто закрыть, то, например, 
-	// вызов system("/bin/ls -l") возвращал ненулевой код ошибки
-    //close(STDOUT_FILENO); 
-    close(STDIN_FILENO);
-    close(STDERR_FILENO);
 }
 
 // считываем команды и файла конфигурации
 void read_config_file() {
-	if(!infile.is_open()) {
+    if(!infile.is_open()) {
         syslog(LOG_ERR, "Failed open");
         exit(EXIT_FAILURE); 
     }
     infile.clear();
-	infile.seekg(0, ios::beg);
+    infile.seekg(0, ios::beg);
     string line;
     while (getline(infile, line)) {
-    	//ищем индекс последнего пробела, чтобы разделить строку
-    	size_t last_space = line.rfind(" ");
-    	string command = line.substr(0, last_space);
-    	string type = line.substr(last_space + 1, line.size() - last_space + 1);
+        //ищем индекс последнего пробела, чтобы разделить строку
+        size_t last_space = line.rfind(" ");
+        string command = line.substr(0, last_space);
+        string type = line.substr(last_space + 1, line.size() - last_space + 1);
 
-    	command_list.push_back(command);
+        command_list.push_back(command);
 
-    	if (type == "wait") 
-    		command_type_list.push_back(0);
-    	else if (type == "respawn")
-    		command_type_list.push_back(1);
-    	else {
-    		syslog(LOG_ERR, "Invalid option");
-        	exit(EXIT_FAILURE); 
-    	}
+        if (type == "wait") 
+            command_type_list.push_back(0);
+        else if (type == "respawn")
+            command_type_list.push_back(1);
+        else {
+            syslog(LOG_ERR, "Invalid option");
+            exit(EXIT_FAILURE); 
+        }
 
     }
 }
 
 //стартуем нашу команду
 int run_command(string command) {
-	double duration = 0; 
+    double duration = 0; 
     struct timespec start, end;
     int attempts = 0;
 
@@ -101,93 +95,93 @@ int run_command(string command) {
 
     //пытаемся запустить до тех пор, пока не будет превышено время в 5 секунд, либо пока не совершим 50 попыток
     while (duration < 5 && attempts++ < 50) {
-    	if (system(command.c_str()) == 0) return EXIT_SUCCESS;
+        if (system(command.c_str()) == 0) return EXIT_SUCCESS;
 
-    	clock_gettime(CLOCK_MONOTONIC, &end);
-    	duration = (end.tv_sec - start.tv_sec) * 1e9; 
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        duration = (end.tv_sec - start.tv_sec) * 1e9; 
         duration = (duration + (end.tv_nsec - start.tv_nsec)) * 1e-9;
     }
     return EXIT_FAILURE;
 }
 
 char* generate_pidfile_name(int num) {
-	char* filename = new char[30];
-	sprintf(filename, "/tmp/fproc%d.pid", num);
-	return filename;
+    char* filename = new char[30];
+    sprintf(filename, "/tmp/fproc%d.pid", num);
+    return filename;
 }
 
 //создаем и записываем pid в файл
 void write_pid_to_file(int num) {
-	ofstream outfile;
-	outfile.open(generate_pidfile_name(num));
-	outfile << pid_list[num] << endl;
+    ofstream outfile;
+    outfile.open(generate_pidfile_name(num));
+    outfile << pid_list[num] << endl;
 }
 
 //запуск нового процесса + старт команды
 void launch_process(int num_command) {
-	pid_t cpid = fork(); 
+    pid_t cpid = fork(); 
 
-	switch (cpid) {
-	  	case -1:
-    		syslog(LOG_ERR, "Fork failed; cpid == -1");
-        	exit(EXIT_FAILURE); 
-	  	case 0:
-    		exit(run_command(command_list[num_command]));
-	  	default:
-	  		pid_list[num_command] = cpid;
-	        pid_count++;
-	  		write_pid_to_file(num_command);
-	}
+    switch (cpid) {
+        case -1:
+            syslog(LOG_ERR, "Fork failed; cpid == -1");
+            exit(EXIT_FAILURE); 
+        case 0:
+            exit(run_command(command_list[num_command]));
+        default:
+            pid_list[num_command] = cpid;
+            pid_count++;
+            write_pid_to_file(num_command);
+    }
 }
 
 //удаление pid-файла
 void remove_pid_file(int num) {
-	char* pidfile = generate_pidfile_name(num);
-	if (access(pidfile, F_OK) != -1 && remove(pidfile) != 0)
-		syslog(LOG_ERR, "Error deleting file: %s", pidfile);
+    char* pidfile = generate_pidfile_name(num);
+    if (access(pidfile, F_OK) != -1 && remove(pidfile) != 0)
+        syslog(LOG_ERR, "Error deleting file: %s", pidfile);
 }
 
 //ожидаем завершения процессов. Если требуется, то перезапускаем
 void wait_processes() {
-	int status;
-	pid_t cpid;
+    int status;
+    pid_t cpid;
 
-	while (pid_count) {
-    	cpid = waitpid(-1, &status, 0);
-    	for (int p = 0; p < pid_list.size(); p++) {
-	        if(pid_list[p] == cpid) {
-	        	//если команда типа "wait"
-	        	if (command_type_list[p] == 0) {
-	        		remove_pid_file(p);
-	        		if (WEXITSTATUS(status) == 0)
-	        			syslog(LOG_INFO, "Completed: '%s', pid = %d", command_list[p].c_str(), cpid);
-	        		else 
-	        			syslog(LOG_INFO, "Failed: '%s', pid = %d", command_list[p].c_str(), cpid);
-	        		pid_count--;
-	        	} 
-	        	else {//иначе если команда типа "respawn"
-	        		remove_pid_file(p);
-	        		//если запуск провалился, то просто уменьшаем число pid'ов
-	        		if (WEXITSTATUS(status) != 0) {
-	        			syslog(LOG_INFO, "Failed: '%s', pid = %d", command_list[p].c_str(), cpid);
-	        			pid_count--;
-	        		} else {//если же запуск был успешным, то запускаем процесс с командой по новой
-	        			syslog(LOG_INFO, "Completed: '%s', pid = %d. And respawn!", command_list[p].c_str(), cpid);
-	        			launch_process(p);
-	        		}
-	        	}
-        	}
-		}
-	}
+    while (pid_count) {
+        cpid = waitpid(-1, &status, 0);
+        for (int p = 0; p < pid_list.size(); p++) {
+            if(pid_list[p] == cpid) {
+                //если команда типа "wait"
+                if (command_type_list[p] == 0) {
+                    remove_pid_file(p);
+                    if (WEXITSTATUS(status) == 0)
+                        syslog(LOG_INFO, "Completed: '%s', pid = %d", command_list[p].c_str(), cpid);
+                    else 
+                        syslog(LOG_INFO, "Failed: '%s', pid = %d", command_list[p].c_str(), cpid);
+                    pid_count--;
+                } 
+                else {//иначе если команда типа "respawn"
+                    remove_pid_file(p);
+                    //если запуск провалился, то просто уменьшаем число pid'ов
+                    if (WEXITSTATUS(status) != 0) {
+                        syslog(LOG_INFO, "Failed: '%s', pid = %d", command_list[p].c_str(), cpid);
+                        pid_count--;
+                    } else {//если же запуск был успешным, то запускаем процесс с командой по новой
+                        syslog(LOG_INFO, "Completed: '%s', pid = %d. And respawn!", command_list[p].c_str(), cpid);
+                        launch_process(p);
+                    }
+                }
+            }
+        }
+    }
 }
 
 void init() {
-	read_config_file();
-	
-	pid_list.resize(command_list.size());
-	for (int p = 0; p < pid_list.size(); p++) {
-		launch_process(p);
-	}
+    read_config_file();
+    
+    pid_list.resize(command_list.size());
+    for (int p = 0; p < pid_list.size(); p++) {
+        launch_process(p);
+    }
 }
 
 //ожидаем сигнал hup
@@ -199,26 +193,30 @@ void hup_handler(int s) {
     for (int p = 0; p < pid_list.size(); p++) {
         kill(pid_list[p], SIGKILL);
         remove_pid_file(p);
-	}
-	command_list.clear();
+    }
+    command_list.clear();
     pid_list.clear();
     command_type_list.clear();
+<<<<<<< c6bbb3a2f2d6dabf05b4cef862e3c20165434eb7
 	pid_count = 0;
+=======
+    pid_count = 0;
+>>>>>>> change daemonize
 
     //по новой инициализируем
     init();
 }
 
 int main(int argc, char const *argv[]) {
-	if (argc != 2) {
+    if (argc != 2) {
         cout << "Invalid arguments" << endl;
         return 0;
     }
     infile.open(argv[1]);
 
-    
     skeleton_daemon();
     syslog(LOG_INFO, "Start");
+<<<<<<< c6bbb3a2f2d6dabf05b4cef862e3c20165434eb7
 	signal(SIGHUP, hup_handler);
 	init();
 	wait_processes();
@@ -226,3 +224,12 @@ int main(int argc, char const *argv[]) {
 	syslog(LOG_INFO, "Successful complete!");
 	return 0; 
 }
+=======
+    signal(SIGHUP, hup_handler);
+    init();
+    wait_processes();
+    infile.close();
+    syslog(LOG_INFO, "Successful complete!");
+    return 0; 
+}
+>>>>>>> change daemonize
